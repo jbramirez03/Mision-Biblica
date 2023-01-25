aee6f6558cd87454a4c44471e1a42b5e4021adb8
temptado <jason1287712@gmail.com>
2023-01-25 03:01:34 -0500

    added components folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>Hello World</>
+    <Router>
+      <Routes>
+        <Route path='/' element={<></>}></Route>
+        <Route path='/about'></Route>
+        <Route path='/studies'></Route>
+        <Route path='/contacts'></Route>
+      </Routes>
+    </Router>
   );
 }
 
