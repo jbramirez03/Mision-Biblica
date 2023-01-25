c503f845f6728c4eb939674b127101b6b18465aa
temptado <jason1287712@gmail.com>
2023-01-25 03:04:55 -0500

    added home path with components in about file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import About from './components/About';
+import Contact from './components/Contact';
+import Studies from './components/Studies';
+import Home from './components/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<></>}></Route>
+        <Route path='/' element={<>
+          <Header />
+          <Home />
+          <Footer></Footer>
+        </>}></Route>
         <Route path='/about'></Route>
         <Route path='/studies'></Route>
         <Route path='/contacts'></Route>
