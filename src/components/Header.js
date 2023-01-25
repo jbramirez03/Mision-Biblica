cf64ee8bb58c372aa40a3e69f9b33179754a5564
temptado <jason1287712@gmail.com>
2023-01-25 04:04:27 -0500

    added functionality to nav toggle button

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,36 +2,46 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Header = () => {
+
+    const navLink = document.querySelectorAll('.nav__link')
+    const [menuState, setMenuState] = React.useState(false);
+    const [scrollState, setScrollState] = React.useState(false);
+
+    function linkAction() {
+        setMenuState(false);
+    }
+    navLink.forEach(n => n.addEventListener('click', linkAction))
+
+    function scrollHeader() {
+        if (this.scrollY >= 80) setScrollState(true);
+        else setScrollState(false);
+    }
+    window.addEventListener('scroll', scrollHeader);
+
     return (
-        <header className='header' id='header'>
+        <header className={`header ${scrollState ? 'scroll-header' : ''}`} id='header'>
             <nav className='nav container'>
                 <Link className='logo nav__logo'>Mísion Biblíca</Link>
-                <div className="nav__menu" id='nav-menu'>
+                <div className={`nav__menu ${menuState ? 'show-menu' : ''}`} id='nav-menu'>
                     <ul className="nav__list">
                         <li className="nav__item">
-                            <Link to='' className='nav__link'></Link>
+                            <Link to='/' className='nav__link'>Home</Link>
                         </li>
                         <li className="nav__item">
-                            <Link to='' className='nav__link'></Link>
+                            <Link to='/About' className='nav__link'>About</Link>
                         </li>
                         <li className="nav__item">
-                            <Link to='' className='nav__link'></Link>
+                            <Link to='/Studies' className='nav__link'>Studies</Link>
                         </li>
                         <li className="nav__item">
-                            <Link to='' className='nav__link'></Link>
-                        </li>
-                        <li className="nav__item">
-                            <Link to='' className='nav__link'></Link>
-                        </li>
-                        <li className="nav__item">
-                            <Link to='' className='nav__link'></Link>
+                            <Link to='/Contact' className='nav__link'>Contact</Link>
                         </li>
                     </ul>
 
-                    <i className="ri-close-line nav__close" id="nav-close"></i>
+                    <i className="ri-close-line nav__close" id="nav-close" onClick={() => setMenuState(false)}></i>
                 </div>
 
-                <div className="nav__toggle" id="nav-toggle">
+                <div className="nav__toggle" id="nav-toggle" onClick={() => setMenuState(true)}>
                     <i className="ri-function-line"></i>
                 </div>
             </nav>
