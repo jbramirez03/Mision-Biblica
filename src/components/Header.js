e5d5da3dfeb8fdc098c25e7461b50815b2f9a1a5
temptado <jason1287712@gmail.com>
2023-01-25 03:24:46 -0500

    added home and header components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,40 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <header id='header'>
+            <nav className='nav container'>
+                <div className="nav__menu" id='nav-menu'>
+                    <ul className="nav__list">
+                        <li className="nav__item">
+                            <Link to='' className='nav__link'></Link>
+                        </li>
+                        <li className="nav__item">
+                            <Link to='' className='nav__link'></Link>
+                        </li>
+                        <li className="nav__item">
+                            <Link to='' className='nav__link'></Link>
+                        </li>
+                        <li className="nav__item">
+                            <Link to='' className='nav__link'></Link>
+                        </li>
+                        <li className="nav__item">
+                            <Link to='' className='nav__link'></Link>
+                        </li>
+                        <li className="nav__item">
+                            <Link to='' className='nav__link'></Link>
+                        </li>
+                    </ul>
+
+                    <i className="ri-close-line nav__close" id="nav-close"></i>
+                </div>
+
+                <div className="nav__toggle" id="nav-toggle">
+                    <i className="ri-function-line"></i>
+                </div>
+            </nav>
+        </header>
     )
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import homeImg from '../images/home.jpg'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className='hero'>
+            <img src={homeImg} alt="Home image" className='hero__img' />
+            <div className="welcome__container">
+                <h1 className="welcome__title"></h1>
+                <h2 className="welcome__subtitle"></h2>
+            </div>
+        </div>
     )
 }
 
