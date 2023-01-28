2fa346ca46bbc3d1c694c41b7e65c833103dbd81
temptado <jason1287712@gmail.com>
2023-01-28 18:09:00 -0500

    added section title and subtitle for studies component

--- src/components/Studies.js
+++ src/components/Studies.js
@@ -5,7 +5,14 @@ import React from 'react'
 const Studies = () => {
 
     return (
-        <></>
+        <section className='studies__section container section' id='studies'>
+            <h2 className="section__title section__title-center">Studies</h2>
+            <h3 className="section__subtitle section__title-center">Consulta nuestras últimas publicaciones</h3>
+
+            <div className="study__posts">
+
+            </div>
+        </section>
     )
 }
 
