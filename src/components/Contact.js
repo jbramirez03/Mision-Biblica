16a5a77b0b73e13ba4bebd51a999b03389ee47ea
temptado <jason1287712@gmail.com>
2023-01-29 23:37:13 -0500

    added contact styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,15 +6,15 @@ const Contact = () => {
 
             <div className="contact__container grid">
                 <div className="contact__box">
-                    <h2 className="section__title section__title-center">Comuníquese con nosotros hoy a través de cualquiera de la información proporcionada</h2>
+                    <h2 className="section__title section__title-center contact__title">Comuníquese con nosotros hoy a través de cualquiera de la información proporcionada</h2>
                     <div className="contact__data">
                         <div className="contact__information">
                             <h3 className="contact__subtitle">Llámenos para soporte instantáneo</h3>
-                            <span className='contact__description'><i class="ri-phone-fill"></i> +1336260771</span>
+                            <span className='contact__description'><i className="ri-phone-fill"></i> +1336260771</span>
                         </div>
                         <div className="contact__information">
                             <h3 className="contact__subtitle">Escríbanos por correo electrónico</h3>
-                            <a href="mailto:imbmebane@gmail.com" className='contact__description'><i class="ri-mail-fill"></i> imbmebane@gmail.com</a>
+                            <a href="mailto:imbmebane@gmail.com" className='contact__description'><i className="ri-mail-fill"></i> imbmebane@gmail.com</a>
                         </div>
                     </div>
                 </div>
@@ -22,47 +22,47 @@ const Contact = () => {
                 <form
                     action="https://formspree.io/f/mnqwkygq"
                     method="POST"
-                    class="contact__form"
+                    className="contact__form"
                 >
-                    <div class="contact__inputs">
-                        <div class="contact__content">
+                    <div className="contact__inputs">
+                        <div className="contact__content">
                             <input
                                 type="email"
-                                class="contact__input"
+                                className="contact__input"
                                 name="email"
                                 id="email"
                                 placeholder=" "
                             />
-                            <label for="email" class="contact__label">Email</label>
+                            <label htmlFor="email" className="contact__label">Email</label>
                         </div>
 
-                        <div class="contact__content">
+                        <div className="contact__content">
                             <input
                                 type="text"
                                 name="subject"
                                 id="subject"
                                 placeholder=" "
-                                class="contact__input"
+                                className="contact__input"
                             />
-                            <label for="subject" class="contact__label">Subject</label>
+                            <label htmlFor="subject" className="contact__label">Subject</label>
                         </div>
 
-                        <div class="contact__content contact__area">
+                        <div className="contact__content contact__area">
                             <textarea
                                 name="message"
                                 placeholder=" "
                                 id="message"
                                 data-gramm_editor="false"
                                 data-enable-grammarly="false"
-                                class="contact__input"
+                                className="contact__input"
                             ></textarea>
-                            <label for="message" class="contact__label">Message</label>
+                            <label htmlFor="message" className="contact__label">Message</label>
                         </div>
                     </div>
 
-                    <button action="submit" class="button button--flex">
+                    <button action="submit" className="button button--flex">
                         Send Message
-                        <i class="ri-arrow-right-up-line button__icon"></i>
+                        <i className="ri-arrow-right-up-line button__icon"></i>
                     </button>
                 </form>
             </div>
