2758d289362ff81f5181a3d7dd993cade1e68c82
temptado <jason1287712@gmail.com>
2023-01-29 22:35:20 -0500

    added contact info box

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,24 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div>Contact</div>
+        <section className="contact__section container section" id="contact">
+            <h2 className="section__title section__title-center">Contact</h2>
+
+            <div className="contact__info grid">
+                <h3 className="contact__title">Comuníquese con nosotros hoy a través de cualquiera de la información proporcionada</h3>
+
+                <div className="contact__box">
+                    <h3 className="contact__subtitle">Llámenos para soporte instantáneo</h3>
+                    <p><i class="ri-phone-fill"></i> +1336260771</p>
+                </div>
+
+                <div className="contact__box">
+                    <h3 className="contact__subtitle">Escríbanos por correo electrónico</h3>
+                    <p><i class="ri-mail-fill"></i> <a href="mailto:imbmebane@gmail.com">imbmebane@gmail.com</a></p>
+                </div>
+
+            </div>
+        </section>
     )
 }
 
