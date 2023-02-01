cc794edc02f79d1db2f70a29296eb236ce7cc92b
temptado <jason1287712@gmail.com>
2023-01-31 21:50:14 -0500

    added media query for desktop view

--- src/components/About.js
+++ src/components/About.js
@@ -7,19 +7,22 @@ const About = () => {
         <section className='section container about__section' id='about'>
             <h2 className="section__title section__title-center">Sobre Nosotros</h2>
 
-            <div className="about__content-1 about__content">
-                <div className="about__information">
-                    <h3>David Ruiz, Pastor</h3>
-                    <p>Somos una famillia de 5 dedicada a servir a nuestra communidad con la palabra de Dios y el espíritu de Dios.</p>
+            <div className="about__blocks">
+
+                <div className="about__content-1 about__content">
+                    <div className="about__information">
+                        <h3>David Ruiz, Pastor</h3>
+                        <p>Somos una famillia de 5 dedicada a servir a nuestra communidad con la palabra de Dios y el espíritu de Dios.</p>
+                    </div>
+                    <img className='about__image' src={aboutImg1} alt="" />
                 </div>
-                <img className='about__image' src={aboutImg1} alt="" />
-            </div>
-            <div className="about__content-2 about__content">
-                <div className="about__information">
-                    <h3>Nuestra Misión</h3>
-                    <p>Amar: Amamos a Dios y a todas personas <br /> Equipar: Nos dedicamos a equipar con la palabra de Dios y ayudarle llegar a ser lo que Dios tiene para su vida</p>
+                <div className="about__content-2 about__content">
+                    <div className="about__information">
+                        <h3>Nuestra Misión</h3>
+                        <p>Amar: Amamos a Dios y a todas personas <br /> Equipar: Nos dedicamos a equipar con la palabra de Dios y ayudarle llegar a ser lo que Dios tiene para su vida</p>
+                    </div>
+                    <img className='about__image' src={aboutImg2} alt="" />
                 </div>
-                <img className='about__image' src={aboutImg2} alt="" />
             </div>
         </section>
     )
