8aecd0ddc82e2c8b5eaa4f48a2d2a22d0bc22c49
temptado <jason1287712@gmail.com>
2023-10-30 23:51:54 -0400

    added new images to about section and reorganized direction

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import aboutImg1 from '../images/About.jpg'
-import aboutImg2 from '../images/about2.jpg'
+import aboutImg1 from '../images/About.jpeg'
+import aboutImg2 from '../images/about1.jpeg'
 
 const About = () => {
     return (
@@ -10,7 +10,7 @@ const About = () => {
             <div className="about__blocks">
 
                 <div className="about__content-1 about__content">
-                    <div className="about__information">
+                    <div className="about__information about__information-1">
                         <h3>David Ruiz,<br /> Pastor</h3>
                         <p>Somos una famillia dedicada a servir a nuestra communidad con la palabra de Dios.</p>
                     </div>
@@ -21,7 +21,7 @@ const About = () => {
                         <h3>Nuestra Misión</h3>
                         <p>Amar: Amamos a Dios y a todas personas <br /> Equipar: Nos dedicamos a equipar con la palabra de Dios <br /> Lanzar: Nuestro deseo es que cada uno se lanze a el proposito de Dios en su vida</p>
                     </div>
-                    <img className='about__image' src={aboutImg2} alt="" />
+                    <img className='about__image about__image-2' src={aboutImg2} alt="" />
                 </div>
             </div>
         </section>
